a773a68290ea0ee096acc7edcba8397af7781b55
Tanja Kantola (local) <tk@Tanja.geometrix.fi>
2019-09-04 16:36:27 +0300

    Part 3 exercises 3.7 and 3.8

---
+++ puhelinluettelo-backend_tasks_from_3.7/mongo.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:lasana12@cluster0-usmgv.mongodb.net/phonebook?retryWrites=true`
+
+mongoose.connect(url, { useNewUrlParser: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: String,
+  number: String,
+})
+
+person.save().then(response => {
+  console.log('person saved!');
+  mongoose.connection.close();
+})
